1bdbfb05d465c8aa224164929c48bd30f9852737
Gabr713l <32576768+Gabr713l@users.noreply.github.com>
2024-01-26 21:39:11 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,76 @@
+#a = 1
+#print(a)
+
+#def teste():
+    #a = 2
+    #print(a)  
+
+#teste()
+#print(a)
+
+# num = float(input('digite um numero:'))
+# def numero_covertido_inteiro(num):
+#     return int(num)
+
+
+# print(numero_covertido_inteiro(num))
+
+
+
+# a = int(input('digite um valor A:'))
+# b = int(input('digite um valor B:'))
+# c = int(input('digite um valor C:'))
+# d = int(input('digite um valor D:'))
+
+# def soma(a,b,c,d):
+#     return a+b+c+d
+
+# print(soma(a,b,c,d))
+
+#end = finalizador
+#sep = separador
+
+# print('ola bb')
+# print("ola", 'bb', 'lindo',end='\n', sep='...')
+# print('fala comigo')
+
+# def funcao (a,b='default'):
+#     return f'{a} {b}'
+# print(funcao(2,3))
+
+#1 - a = input(palavra'digite uma palavra para imprimir:')
+# def imprima():
+#     return {pala
+# print(imprima)
+
+
+#2 - a = float(input('digite um valor'))
+# def valor_inteiro(valor):
+#     return (a) % 2 == 0
+
+# print(valor_inteiro(a))
+#obs: preciso treinar mais
+    
+#3 - valor1 = float(input('digite um numero:'))
+# valor2 = float(input('digite um numero:'))
+
+# def numeros_reais(valor1,valor2):
+#     if a>b:
+#         return 'a maior que b'
+#     else:
+#         return'b maior que a'
+    
+
+# print(valor1,valor2)    
+
+def remov_vogal(frase):
+    resultado = ''
+    
+resultado = (letra for letra in 
+    
+
+
+
+
+
+      
